583d66064749e41625397b3a21935de710728124
User <User@LAPTOP-3MJSQ1I4>
2024-09-16 15:28:55 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication2/ConsoleApplication2.cpp
@@ -0,0 +1,71 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+	int a1 = 60;
+	int a2 = 60;
+	int b1 = 25;
+	int c1 = a1 + b1;
+	int c2 = a1 - b1;
+	int c3 = a1 * b1;
+	int c4 = a1 / b1;
+	int c5 = a1 % b1;
+
+	if (a1 > b1)
+	{
+		cout << "true" << endl;
+	}
+	else {
+		cout << "false" << endl;
+	}
+
+
+	if (c1 < c3)
+	{
+		cout << "true" << endl;;
+	}
+	else {
+		cout << "false" << endl;
+	}
+
+
+	if (c4 != c5)
+	{
+		cout << "true" << endl;
+	}
+	else {
+		cout << "false" << endl;
+	}
+
+
+	if (a1 == a2)
+	{
+		cout << "true" << endl;
+	}
+	else {
+		cout << "false" << endl;
+	}
+
+
+
+	if (a1 > b1 && c2 < c1)
+	{
+		cout << "true" << endl;
+	}
+	else {
+		cout << "false" << endl;
+	}
+
+
+
+	if (c5 < c3 || c5 > c3)
+	{
+		cout << "true" << endl;
+	}
+	else {
+		cout << "false" << endl;
+	}
+
+
+}
